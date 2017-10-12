2fd6a8e5d9e39dbfa2a79bcebd5905b448c560e6
ShervenLee <shervenlee@163.com>
2017-10-12 22:24:03 +0800

    change

--- src/cn/sherven/doraemon/test/Test.java
+++ src/cn/sherven/doraemon/test/Test.java
@@ -1,21 +1,36 @@
 package cn.sherven.doraemon.test;
 
-import static org.hamcrest.CoreMatchers.nullValue;
 
 import java.util.List;
 
+import org.hibernate.Session;
+
+import cn.sherven.doraemon.hibernate.HibernateUtils;
 import cn.sherven.doraemon.hibernate.UserStudentH;
 import cn.sherven.doraemon.hibernateController.UserStudent_HC;
 
 public class Test {
 
 	public static void main(String[] args) {
+		
+		Session session=HibernateUtils.getSession();
+		session.beginTransaction();
+		
+		
 		List<UserStudentH> search = UserStudent_HC.search("");
 		if (search != null) {
 			for (int i = 0; i < search.size(); i++) {
-				System.out.println(search.get(i).getName());
+				System.out.println(search.get(i).getName()+"--"+i);
+				
+				search.get(i).setMsgtable_id("1");
+				session.update(search.get(i));
+//				session.getTransaction().commit();
 			}
 		}
+		
+		session.getTransaction().commit();
+		session.close();
+		
 		System.exit(0);
 	}
 

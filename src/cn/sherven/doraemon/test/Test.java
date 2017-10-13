f8c5d64547dd0cfe08ce78d3553e005aeefde43e
ShervenLee <shervenlee@163.com>
2017-10-13 20:49:55 +0800

    change

--- src/cn/sherven/doraemon/test/Test.java
+++ src/cn/sherven/doraemon/test/Test.java
@@ -1,37 +1,23 @@
 package cn.sherven.doraemon.test;
 
-
 import java.util.List;
 
-import org.hibernate.Session;
 
-import cn.sherven.doraemon.hibernate.HibernateUtils;
-import cn.sherven.doraemon.hibernate.UserStudentH;
-import cn.sherven.doraemon.hibernateController.UserStudent_HC;
+import com.google.gson.Gson;
+
+import cn.sherven.doraemon.admin.Model.MessageTableModel;
+import cn.sherven.doraemon.admin.jsonclass.JsonCommentTable;
+import cn.sherven.doraemon.hibernate.MessageTableH;
+import cn.sherven.doraemon.hibernateController.MessageTable_HC;
 
 public class Test {
 
 	public static void main(String[] args) {
-		
-		Session session=HibernateUtils.getSession();
-		session.beginTransaction();
-		
-		
-		List<UserStudentH> search = UserStudent_HC.search("");
-		if (search != null) {
-			for (int i = 0; i < search.size(); i++) {
-				System.out.println(search.get(i).getName()+"--"+i);
-				
-				search.get(i).setMsgtable_id("1");
-				session.update(search.get(i));
-//				session.getTransaction().commit();
-			}
+		try {
+			System.out.println(MessageTableModel.del("3"));
+		} finally {
+			System.exit(0);
 		}
-		
-		session.getTransaction().commit();
-		session.close();
-		
-		System.exit(0);
 	}
 
 }

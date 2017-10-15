9c5a885360cdb54996df991717f668da5c977013
ShervenLee <shervenlee@163.com>
2017-10-15 14:31:36 +0800

    change

--- src/cn/sherven/doraemon/test/Test.java
+++ src/cn/sherven/doraemon/test/Test.java
@@ -1,23 +1,9 @@
 package cn.sherven.doraemon.test;
 
-import java.util.List;
-
-
-import com.google.gson.Gson;
-
-import cn.sherven.doraemon.admin.Model.MessageTableModel;
-import cn.sherven.doraemon.admin.jsonclass.JsonCommentTable;
-import cn.sherven.doraemon.hibernate.MessageTableH;
-import cn.sherven.doraemon.hibernateController.MessageTable_HC;
-
 public class Test {
 
 	public static void main(String[] args) {
-		try {
-			System.out.println(MessageTableModel.del("3"));
-		} finally {
-			System.exit(0);
-		}
+		System.exit(0);
 	}
 
 }
